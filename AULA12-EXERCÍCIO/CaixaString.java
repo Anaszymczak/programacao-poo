d5ce12f57d401d94648f0b6c0dc3007653f0db63
Ana Paula Szymczak <anapaulaszymczak@outlook.com.br>
2025-06-11 20:11:11 -0300

    Update aula

---
+++ AULA12-EXERCÍCIO/CaixaString.java
@@ -0,0 +1,15 @@
+// Exemplo de classe imutável
+// - Depois da criação não permite mais alteração dos atributos
+// - Usado bastante em programação funcionalista
+public class CaixaString {
+
+    private String valor;
+
+    public CaixaString(String valor) {
+        this.valor = valor;
+    }
+
+    public String getValor() {
+        return valor;
+    }
+}
---
+++ AULA12-EXERCÍCIO/Util.java
@@ -0,0 +1,18 @@
+public class Util {
+
+    public static final int MAX_TAM = 10;
+    public static final String IDIOMA = "pt-BR";
+
+    private Util() {
+        // Construtor privado - impede que a classe seja
+        // instanciada ou herdada
+        // Deve possuir apenas constantes e funções de uso
+        // geral
+    }
+
+    public static String limpaString(String texto) {
+        return texto.strip().toUpperCase();
+    }
+
+
+}
